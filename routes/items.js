d3e97a99a2579aff16e51482692556bf4e3729b7
BibianaBalBar <balbuenabibi@gmail.com>
2020-12-28 13:13:01 +0000

    update and delete item routes

--- routes/items.js
+++ routes/items.js
@@ -60,15 +60,56 @@ router.post(
 // @route     PUT api/items/:id
 // @desc      Update item
 // @access    Private
-router.put('/:id', (req, res) => {
-  res.send('Update item');
+router.put('/:id', auth, async (req, res) => {
+  const { name, description, code, quantity, type } = req.body;
+
+  //Build item object
+  const itemFields = {};
+  if(name) itemFields.name = name;
+  if(description) itemFields.description = description;
+  if(code) itemFields.code = code;
+  if(quantity) itemFields.quantity = quantity;
+  if(type) itemFields.type = type;
+
+  try {
+    let item = await Item.findById(req.params.id);
+    if(!item) return res.status(404).json({ msg: 'Item not found' });
+
+    // Make sure user owns items
+    if(item.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    item = await Item.findByIdAndUpdate(req.params.id,
+      { $set: itemFields },
+      { new: true }
+    );
+    res.json(item);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route     DELETE api/items/:id
 // @desc      Delete item
 // @access    Private
-router.delete('/:id', (req, res) => {
-  res.send('Delete item');
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let item = await Item.findById(req.params.id);
+    if(!item) return res.status(404).json({ msg: 'Item not found' });
+
+    // Make sure user owns items
+    if(item.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    await Item.findByIdAndRemove(req.params.id);
+    res.json({ msg: 'Item removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
