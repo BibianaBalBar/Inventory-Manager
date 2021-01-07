469e1a6ca1b404d3cb8a21caf2e72648e7b66838
BibianaBalBar <balbuenabibi@gmail.com>
2021-01-07 14:02:54 +0000

    alert context, reducer and state

--- client/src/App.js
+++ client/src/App.js
@@ -7,25 +7,28 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import ItemState from './context/item/ItemState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
   return (
     <AuthState>
       <ItemState>
-        <Router>
-          <Fragment className="App">
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment className="App">
+              <Navbar />
+              <div className="container">
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ItemState>
     </AuthState>
   );
