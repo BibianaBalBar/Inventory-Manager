86a3f7dbbf396b958a40f3edefc27decb5a0043c
BibianaBalBar <balbuenabibi@gmail.com>
2021-01-13 15:55:01 +0000

    user login

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -18,7 +18,8 @@ export default (state, action) => {
         loading: false,
         user: action.payload
       };   
-    case REGISTER_SUCCESS:    
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:    
       localStorage.setItem('token', action.payload.token);
       return {
         ...state,
@@ -27,7 +28,8 @@ export default (state, action) => {
         loading: false
       };
     case REGISTER_FAIL:
-    case AUTH_ERROR:    
+    case AUTH_ERROR:  
+    case LOGIN_FAIL:  
       localStorage.removeItem('token');
       return {
         ...state,
